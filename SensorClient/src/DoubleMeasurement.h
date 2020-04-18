c447a375471bf9b0e7871102e786ca2a4810fbd4
mzadroz1 <01133395@pw.edu.pl>
2020-04-18 18:29:49 +0200

    Double measurement

---
+++ SensorClient/src/DoubleMeasurement.h
@@ -0,0 +1,27 @@
+//
+// Created by Michal on 18.04.2020.
+//
+
+#ifndef SENSOR_DOUBLEMEASUREMENT_H
+#define SENSOR_DOUBLEMEASUREMENT_H
+
+
+#include "IMeasurement.h"
+
+class DoubleMeasurement : public IMeasurement {
+public:
+
+    DoubleMeasurement(long timestamp, double measurement);
+
+    long getTimestamp() override;
+
+    std::vector<unsigned char> getBytes() override;
+
+    int setValue(std::vector<unsigned char> bytes) override;
+
+private:
+    double measurement;
+};
+
+
+#endif //SENSOR_DOUBLEMEASUREMENT_H
