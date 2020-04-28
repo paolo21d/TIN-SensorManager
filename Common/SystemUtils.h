d80bc58b33bbd4f97d4b3480086e0a420d5a46c0
mzadroz1 <01133395@pw.edu.pl>
2020-04-28 22:55:29 +0200

    Windows fix

--- Common/SystemUtils.h
+++ Common/SystemUtils.h
@@ -1,7 +1,11 @@
 #ifndef SERVER_SYSTEMUTILS_H
 #define SERVER_SYSTEMUTILS_H
 
+#ifdef WIN32
+#include<windows.h>
+#else
 #include <unistd.h>
+#endif
 
 void sleepSecs(int secs);
 void sleepMillis(int millis);
